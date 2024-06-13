e6425510a3e8d629170accafa4718ea8110a5f29
arukanus <110685861+arukanus@users.noreply.github.com>
2024-06-14 04:27:23 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class', // Enable dark mode support
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
